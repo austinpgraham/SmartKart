7b16a2db7ad5ec53af1cfe3437ba587394a35af6
Austin Graham <austinpgraham@Austins-MacBook-Pro.local>
2017-09-27 16:07:22 -0500

    auto navigate to race

---
+++ src/smartkart/action/KeyPress.java
@@ -0,0 +1,48 @@
+package smartkart.action;
+
+import java.awt.AWTException;
+import java.awt.Robot;
+
+/*
+ * Abstraction of the Java AWT key pressing
+ * technology. Just so we don't have to do 
+ * three things for every key press.
+ */
+public class KeyPress
+{
+	private Robot r;
+	
+	public KeyPress() throws AWTException
+	{
+		r = new Robot();
+	}
+	
+	public void pressKey(int keyCode)
+	{
+		this.r.keyPress(keyCode);
+		this.wait(0.5f);
+		this.r.keyRelease(keyCode);
+	}
+	
+	public void longPressKey(int keyCode)
+	{
+		this.r.keyPress(keyCode);
+		this.wait(1f);
+		this.r.keyRelease(keyCode);
+	}
+	
+	public void pressAndHold(int keyCode)
+	{
+		this.r.keyPress(keyCode);
+	}
+	
+	public void release(int keyCode)
+	{
+		this.r.keyRelease(keyCode);
+	}
+	
+	public void wait(float seconds)
+	{
+		this.r.delay((int)(seconds * 1000));
+	}
+}
---
+++ src/smartkart/startup/RaceStart.java
@@ -0,0 +1,44 @@
+package smartkart.startup;
+
+import java.awt.AWTException;
+import java.awt.event.KeyEvent;
+
+import smartkart.action.KeyPress;
+
+class RaceStart
+{
+	
+	public static void navigateToRace() throws AWTException
+	{
+		KeyPress kp = new KeyPress();
+		// Wait for focus on the emulator
+		kp.wait(5f);
+		
+		// Go through start screen
+		kp.pressKey(KeyEvent.VK_ENTER);
+		kp.wait(7f);
+		kp.pressKey(KeyEvent.VK_ENTER);
+		kp.wait(7f);
+		
+		// Select Grand Prix / 50 CC
+		kp.pressKey(KeyEvent.VK_ENTER);
+		kp.wait(1f);
+		kp.pressKey(KeyEvent.VK_ENTER);
+		kp.wait(1f);
+		kp.pressKey(KeyEvent.VK_ENTER);
+		kp.wait(1f);
+		kp.pressKey(KeyEvent.VK_ENTER);
+		kp.wait(3f);
+		
+		// Select Mario
+		kp.pressKey(KeyEvent.VK_ENTER);
+		kp.wait(2f);
+		kp.pressKey(KeyEvent.VK_ENTER);
+		kp.wait(2f);
+		
+		// Select Mushroom Cup
+		kp.pressKey(KeyEvent.VK_ENTER);
+		kp.wait(1f);
+		kp.pressKey(KeyEvent.VK_ENTER);
+	}
+}
