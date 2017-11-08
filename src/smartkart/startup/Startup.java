66d5ba1ed2f8e33f9828cc120d6c9967a642e935
Austin Graham <austingraham@Austins-MacBook-Pro.local>
2017-11-08 15:39:11 -0800

    more fixes

--- src/smartkart/startup/Startup.java
+++ src/smartkart/startup/Startup.java
@@ -30,15 +30,11 @@ import smartkart.action.GasPedal;
  */
 public class Startup
 {
-<<<<<<< HEAD
-	final static int EPISODES = 1000;
-=======
 	/*Declare various configuration parameters*/
 	final static int EPISODES = 10000;
->>>>>>> master
 	final static double LEARNING_RATE = 0.01;
 	final static double GAMMA = 0.6; 
-	final static long REALLYFUKINBIGTEMP = (long) 10e17;
+	final static long REALLYBIGTEMP = (long) 10e17;
 	final static float TEMP = 0.5f;
 	
 	static float EPSILON = 0.3f;
@@ -57,12 +53,8 @@ public class Startup
 	static ArrayList<Integer> rewards = new ArrayList<Integer>();
 	static ArrayList<Integer> states = new ArrayList<Integer>();
 	
-<<<<<<< HEAD
+	/*Q-Table for relevant experiments*/
 	static float[][] qTable = new float[STATES][ACTIONS];
-=======
-	/*Create Q-Table for relevant approaches*/
-	static float[][] qTable = new float[3][5];
->>>>>>> master
 	
 	/*Signal to press the gas pedal*/
 	public static GasPedalComponent hitGas()
@@ -84,15 +76,15 @@ public class Startup
 		return (Math.random() < EPSILON) ? (int)(Math.random()*5) : predictedValue;
 	}
 	
-<<<<<<< HEAD
+	/*Softmax action selection*/
 	public static int softmax(float[] qValues) {
 		float sum = 0;
 		for(float f: qValues) {
-			sum += Math.exp(f/REALLYFUKINBIGTEMP);
+			sum += Math.exp(f/REALLYBIGTEMP);
 		}
 		ArrayList<Integer> select = new ArrayList<Integer>();
 		for(int i = 0; i < qValues.length; i++) {
-			int repeat = (int)((Math.exp(qValues[i]/REALLYFUKINBIGTEMP) / sum)*100);
+			int repeat = (int)((Math.exp(qValues[i]/REALLYF=BIGTEMP) / sum)*100);
 			for (int j = 0; j < repeat; j++) {
 				select.add(i);
 			}
@@ -101,9 +93,7 @@ public class Startup
 		
 	}
 	
-=======
 	/*Perform the designated action*/
->>>>>>> master
 	public static void doAction(int action) {
 		switch(action) {
 		case 0:
@@ -147,7 +137,7 @@ public class Startup
 		return reward;
 	}
 	
-<<<<<<< HEAD
+	/*Get reward with updated structure*/
 	public static int getMarioReward(ImageInput state) {
 		String marioState = state.getState();
 		int reward = 0;
@@ -167,6 +157,7 @@ public class Startup
 		return reward;
 	}
 		
+	/*Convert the state string to int*/
 	public static int stateToInt(String state) {
 		switch(state) {
 		case "onRoad":
@@ -183,9 +174,7 @@ public class Startup
 		return -1;
 	}
 	
-=======
 	/*Deep-Q-Learning*/
->>>>>>> master
 	public static ImageInput DQNLearning(ImageInput state, ConvolutionalNetwork agent) {
 		// If no state, get next state and continue
 		if(state == null) {
@@ -194,27 +183,15 @@ public class Startup
 		}
 		// Feed the current state
 		LearningResult results = agent.feedNetwork(state);
-<<<<<<< HEAD
-		//int nextAction = chooseAction(results.getPredictedValue());
-		int nextAction = softmax(results.getQValues());
-=======
 		// Pick the action
 		int nextAction = chooseAction(results.getPredictedValue());
 		// Do the action
->>>>>>> master
 		doAction(nextAction);
 		// Get the next state
 		ImageInput newState = getCurrentState();
-<<<<<<< HEAD
 		states.add(stateToInt(newState.getState()));
 		LearningResult secondResult = agent.feedNetwork(newState);
 		int reward = getMarioReward(newState);
-=======
-		// Re-feed to get new state information
-		LearningResult secondResult = agent.feedNetwork(newState);
-		int reward = getReward(newState);
-		// Calculate expected Q versus predicted
->>>>>>> master
 		float[] target = results.getQValues();
 		float[] second = secondResult.getQValues();
 		target[results.getPredictedValue()] = (float) (reward + GAMMA*second[secondResult.getPredictedValue()]);
